df78fadcb89fe5c3495bfe7e20816b86f9a68734
santaK1aus <mrigankamanna2@gmail.com>
2022-09-08 02:38:46 -0700

    compl tuple q5

--- Python/itple.py
+++ Python/itple.py
@@ -4,6 +4,7 @@ print(t)
 inp=int(input('Enter value to search : '))
 pos=t.index(inp) #raises ValueError if item not found
 print('Found at',pos)
+pos=t.index(inp,3,4) #start and stop index
 str='Hello there'
 li=list(str)
 print(li)
