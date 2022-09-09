241e515efa6cbe9ac45ad1da33b80cd9bf25e7c6
Mriga <mrigankamanna3@gmail.com>
2022-09-09 21:46:49 +0530

    Add col.py

---
+++ Python/Dictionary/col.py
@@ -0,0 +1,5 @@
+#8.4
+colors=['Red','Blue','Green','Yellow','White','Purple']
+codes=['FF0000','0000FF','00FF00','FFFF00','FFFFFF','800080']
+colco={colors[i]:codes[i] for i in range(0,len(colors))}
+print(colco)
