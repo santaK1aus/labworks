cb5d171aef528ff5d14ae950e079d80311619e18
santaK1aus <mrigankamanna2@gmail.com>
2022-09-21 11:39:00 +0530

    Done QLL using rear and front

--- C/queuell.c
+++ C/queuell.c
@@ -6,28 +6,59 @@ struct Node{
 	struct Node *next;
 };
 
-struct Node* append(struct Node* start,int val){
-	struct Node *ptr=start,*new=(struct Node*)malloc(sizeof(struct Node));
+struct Node *front=NULL,*rear=NULL;
+
+/*struct Node* append(struct Node* front,struct Node* rear,int val){
+	struct Node *new=(struct Node*)malloc(sizeof(struct Node));
 	new->data=val;
 	new->next=NULL;
-	if(start==NULL)
+	if(rear==NULL){
+		front=new;
 		return new;
-	while(ptr->next!=NULL)
-		ptr=ptr->next;
-	ptr->next=new;
-	return start;
+	}
+	rear->next=new;
+	return new;
 }
 
-struct Node* pop(struct Node *start){
-	if(start==NULL){
+struct Node* pop(struct Node *front,struct Node *rear){
+	if(front==NULL){
 		printf("\nUnderflow\n\n");
-		return start;
+		return front;
 	}
 	else{
-		printf("\nPopped %d\n\n",start->data);
-		struct Node *temp=start->next;
-		free(start);
+		printf("\nPopped %d\n\n",front->data);
+		struct Node *temp=front->next;
+		free(front);
+		if(temp==NULL)
+			rear==NULL;
 		return temp;
+	}
+}*/
+
+void append(int val){
+	struct Node *new=(struct Node*)malloc(sizeof(struct Node));
+	new->data=val;
+	new->next=NULL;
+	if(rear==NULL){
+		front=new;
+		rear=new;
+		return;
+	}
+	rear->next=new;
+	rear=new;
+}
+
+void pop(){
+	if(front==NULL){
+		printf("\nUnderflow\n\n");
+	}
+	else{
+		printf("\nPopped %d\n\n",front->data);
+		struct Node *temp=front->next;
+		free(front);
+		if(temp==NULL)
+			rear=NULL;
+		front=temp;
 	}
 }
 
@@ -45,7 +76,7 @@ void display(struct Node *start){
 }
 
 void main(){
-	struct Node *start=NULL;
+	//struct Node *front=NULL,*rear=NULL;
 	int ch=-1,inp;
 	while(ch!=4){
 		printf("\n1.Append\n2.Pop\n3.Display\n4.Exit\nEnter choice : ");
@@ -53,11 +84,13 @@ void main(){
 		switch(ch){
 			case 1:printf("Enter value : ");
 				scanf("%d",&inp);
-				start=append(start,inp);
+				//rear=append(front,rear,inp);
+				append(inp);
 				break;
-			case 2:start=pop(start);
+			case 2://front=pop(front,rear);
+				pop();
 				break;
-			case 3:display(start);break;
+			case 3:display(front);break;
 			case 4:break;
 			default:printf("\nWrong input\n\n");
 		}
