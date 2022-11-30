51c4b29fbd74eaec6b23a18fa1aa55c9afb17d59
keiran <mrigankamanna3@gmail.com>
2022-11-30 19:11:33 +0530

    Finished GUI

--- Python/GUI/mouse.py
+++ Python/GUI/mouse.py
@@ -1,27 +1,31 @@
-from tkinter import *
+from tkinter import *;
 
 var = Tk()
+var.title("Click ME !")
 
 def leftclick(event):
-
-    print("left")
+    tx.delete('1.0','end')
+    tx.insert(END, 'Left Click')
+    #print("left")
 
 def middleclick(event):
-
-    print("middle")
+    tx.delete('1.0','end')
+    tx.insert(END, 'Middle Click')
+    #print("middle")
 
 def rightclick(event):
-
-    print("right")
+    tx.delete('1.0','end')
+    tx.insert(END, 'Right Click')
+    #print("right")
 
 frame = Frame(var, width=300, height=250)
 
+tx=Text(var,height = 1, width = 15, bg = 'light yellow')
+tx.pack()
+
 frame.bind("<Button-1>", leftclick)
-
 frame.bind("<Button-2>", middleclick)
-
 frame.bind("<Button-3>", rightclick)
-
 frame.pack()
 
 var.mainloop()
